1f7a435112c3d87de1745e7bc2430b9cdf6cf045
甜檸Cirtron <45784494+lcandy2@users.noreply.github.com>
2024-12-14 19:44:05 +0800

    Init with default template

---
+++ NeoDB/NeoDB/NeoDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeoDBApp.swift
+//  NeoDB
+//
+//  Created by citron on 12/14/24.
+//
+
+import SwiftUI
+
+@main
+struct NeoDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
