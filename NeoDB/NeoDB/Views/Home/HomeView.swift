2555039b94e20c4b1a67da58672987512c94a8c5
甜檸Cirtron <45784494+lcandy2@users.noreply.github.com>
2025-01-07 15:39:16 +0800

    fix: wrong logger substem

--- NeoDB/NeoDB/Views/Home/HomeView.swift
+++ NeoDB/NeoDB/Views/Home/HomeView.swift
@@ -4,7 +4,7 @@ import OSLog
 @MainActor
 class HomeViewModel: ObservableObject {
     private let timelineService: TimelineService
-    private let logger = Logger(subsystem: "social.neodb.app", category: "HomeViewModel")
+    private let logger = Logger(subsystem: "app.neodb", category: "HomeViewModel")
     
     @Published var statuses: [Status] = []
     @Published var isLoading = false
