10ef76ecde613a7cf4c9b3590c719dadafed5620
甜檸Cirtron <45784494+lcandy2@users.noreply.github.com>
2025-01-13 17:23:12 +0800

    refactor: update NetworkEndpoint and TimelineEndpoint for improved pagination and clarity
    - Refactored `NetworkEndpoint` to enhance the structure and readability of the code, including the addition of a new `makePaginationParam` function for better pagination handling.
    - Updated `TimelineEndpoints` to `TimelineEndpoint`, consolidating cases and improving parameter handling for fetching timelines.
    - Simplified query item generation in `TimelineEndpoint` to utilize the new pagination method, enhancing code maintainability and clarity.

--- NeoDB/NeoDB/Services/Network/Endpoints/TimelineEndpoints.swift
+++ NeoDB/NeoDB/Services/Network/Endpoints/TimelineEndpoints.swift
@@ -1,32 +1,30 @@
 import Foundation
 
-enum TimelineEndpoints {
-    case fetchTimeline(page: Int)
-    case fetchLocalTimeline(page: Int)
+enum TimelineEndpoint {
+    case pub(sinceId: String?, maxId: String?, minId: String?, local: Bool)
+    case home(sinceId: String?, maxId: String?, minId: String?)
 }
 
-extension TimelineEndpoints: NetworkEndpoint {
+extension TimelineEndpoint: NetworkEndpoint {
     var path: String {
         switch self {
-        case .fetchTimeline:
-            return "/timeline"
-        case .fetchLocalTimeline:
-            return "/timeline/local"
+        case .pub:
+            return "/timelines/public"
+        case .home:
+            return "/timelines/home"
         }
     }
-    
-    var method: HTTPMethod {
-        return .get
-    }
-    
     var queryItems: [URLQueryItem]? {
         switch self {
-        case .fetchTimeline(let page), .fetchLocalTimeline(let page):
-            return [URLQueryItem(name: "page", value: String(page))]
+        case .pub(let sinceId, let maxId, let minId, let local):
+            var params =
+                makePaginationParam(
+                    sinceId: sinceId, maxId: maxId, mindId: minId) ?? []
+            params.append(.init(name: "local", value: local ? "true" : "false"))
+            return params
+        case .home(let sinceId, let maxId, let mindId):
+            return makePaginationParam(
+                sinceId: sinceId, maxId: maxId, mindId: mindId)
         }
     }
-    
-    var body: Data? {
-        return nil
-    }
-} 
+}
