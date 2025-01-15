3891655b3f9d86baf875e02eec2304705509195d
甜檸Cirtron <45784494+lcandy2@users.noreply.github.com>
2025-01-15 12:18:21 +0800

    feat: introduce Item and ItemCategory models for enhanced media representation
    - Added Item.swift to define the base structure for various media types, including properties for title, description, and external resources.
    - Introduced ItemCategory.swift to categorize different media types such as book, movie, and podcast, improving organization and type safety.
    - Updated Models.swift to comment out previous model definitions, preparing for the new structure while maintaining backward compatibility.

---
+++ NeoDB/NeoDB/Services/Models/Network/Item/ItemCategory.swift
@@ -0,0 +1,22 @@
+//
+//  ItemCategory.swift
+//  NeoDB
+//
+//  Created by citron on 1/15/25.
+//
+
+enum ItemCategory: String, Codable, CaseIterable {
+    case book
+    case movie
+    case tv
+    case tvSeason
+    case tvEpisode
+    case music
+    case game
+    case podcast
+    case performance
+    case performanceProduction
+    case fanfic
+    case exhibition
+    case collection
+}
